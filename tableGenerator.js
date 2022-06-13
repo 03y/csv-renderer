9bc419c048c36c16be91a20fbbc7052a9c696f33
03y <98747393+03y@users.noreply.github.com>
2022-06-13 17:56:51 +0100

    fix

--- tableGenerator.js
+++ tableGenerator.js
@@ -1,22 +1,22 @@
 function upload() {
-    var fileUpload = document.getElementById("fileUpload");
-    document.getElementById("name").innerHTML = fileUpload.files[0].name;
+    var fileUpload = document.getElementById('fileUpload');
+    document.getElementById('name').innerHTML = fileUpload.files[0].name;
     var reader = new FileReader();
     reader.onload = function (e) {
-        var table = document.createElement("table");
-        var rows = e.target.result.split("\n");
+        var table = document.createElement('table');
+        var rows = e.target.result.split('\n');
         for (var i = 0; i < rows.length; i++) {
-            var cells = rows[i].split("\",");
+            var cells = rows[i].split('\",');
             if (cells.length > 1) {
                 var row = table.insertRow(-1);
                 for (var j = 0; j < cells.length; j++) {
                     var cell = row.insertCell(-1);
-                    cell.innerHTML = cells[j].replace(/\"/g, "");
+                    cell.innerHTML = cells[j].replace(/\"/g, '');
                 }
             }
         }
-        var dvCSV = document.getElementById("table");
-        dvCSV.innerHTML = "";
+        var dvCSV = document.getElementById('table');
+        dvCSV.innerHTML = '';
         dvCSV.appendChild(table);
     }
     reader.readAsText(fileUpload.files[0]);
